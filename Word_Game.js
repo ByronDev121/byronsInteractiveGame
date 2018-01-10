5abcc91f88372380cc62d44d2e354ec1508aec67
Byron de Villiers <byron.div@gmail.com>
2018-01-10 11:27:52 +0200

    Updated .js file after code review with Jacques de Villiers

--- Word_Game.js
+++ Word_Game.js
@@ -1,132 +1,123 @@
-// JavaScript for interative game:
+var questionNumber = 0;
+var questionsCorrect = 0;
+var rank = 'Bronze';
 
-var questionNumber = 1;
-var questionAsked = "What is the day today";
-var questionCorrect = 0;
-var Rank = 'Bronze';
-var correctAnswer1 = "";
-var correctAnswer2 = "";
+function submitAnswer() {
+	validateAnswer();
+	clearAnswerInputField();
+	updateRank();
 
-
-function getQuestion(questionNumber){
-	
-	if (questionNumber == 2) {
-		questionAsked = 'What is: 2 + 2 - 1 = ?  (Quick Mafs) [' + questionCorrect +' Correct Answers - Current Rank = ' + Rank + ' ]'
+	if (isGameEnded()) {
+		endGame();
+	} else {
+		updateNextQuestion();
 	}
-	
-	if (questionNumber == 3) {
-		questionAsked = 'What animal goes moo? [' + questionCorrect +' Correct Answers - Current Rank = ' + Rank + ' ]'
-	}
-	
-	if (questionNumber == 4) {
-		questionAsked = 'How many fingers do humans have on one hand? [' + questionCorrect +' Correct Answers - Current Rank = ' + Rank + ' ]'
-	}
-	
-	if (questionNumber == 5) {
-		questionAsked = 'Who sings "I feel good"? [' + questionCorrect +' Correct Answers - Current Rank = ' + Rank + ' ]'
-	}
-	
-	if (questionNumber == 6 && Rank == "Bronze") {
-		endGame()
-		questionAsked = 'Dam Daniel! Go back to high school! You answered ' + questionCorrect +' questions correctly and earned a Rank of - ' + Rank;
-	}
-	
-	if (questionNumber == 6 && Rank == "Silver"){
-		endGame()
-		questionAsked = 'Not Bad! You answered ' + questionCorrect +' questions correctly and earned a Rank of - ' + Rank;
-	}
-	
-	if (questionNumber == 6 && Rank == "Gold"){
-		endGame()
-		questionAsked = 'Awesome! You answered ' + questionCorrect +' questions correctly and earned a Rank of - ' + Rank;
-	}
-	
-	question = document.getElementById('question');
-	question.innerHTML = questionAsked;
 }
 
+function validateAnswer() {
+	var answer = document.getElementById('answer').value.toLowerCase();
 
-function answerQuestion() {
-	
-	var answer = document.getElementById('answer').value;
-	
-	if (questionNumber == 1){
-		var d = new Date();
-		var days = ["sunday","monday","tuesday","wednesday","thursday","friday","saturday"];
-		correctAnswer1 = (days[d.getDay()]);
-		
+	var correctTextAnswers = [getTodayAsString(), "three", "cow", "five", "james brown"];
+	var correctIntAnswers = [null, 3, null, 5, null];
+
+	if (answer === correctTextAnswers[questionNumber - 1] ||
+		parseInt(answer) === correctIntAnswers[questionNumber - 1]) {
+		showAnswerResultAlert(true);
+		questionsCorrect += 1;
 	}
-	else if (questionNumber == 2){
-		correctAnswer1 = "three";
-		correctAnswer2 = 3;
-	}
-	else if (questionNumber == 3){
-		correctAnswer1 = "cow";
-	}
-	else if (questionNumber == 4){
-		correctAnswer1 = "five";
-		correctAnswer2 = 5;
-	}
-	else if (questionNumber == 5){
-		correctAnswer1 = "james brown";
-	}
-	if ( answer.toLowerCase() === correctAnswer1 || parseInt(answer) === correctAnswer2) {
-		var failAlert = document.getElementById('fail-alert-box');
-		failAlert.classList.add("hide");
-		var successAlert = document.getElementById('success-alert-box');
-		successAlert.classList.remove("hide");
-		questionCorrect += 1;
-		if (questionCorrect < 5 && questionCorrect > 2) {
-			Rank = 'Silver';
-		}
-		else if (questionCorrect === 5){
-			Rank = 'Gold';
-		 }
-    }
 	else {
-		var successAlert = document.getElementById('success-alert-box');
-		successAlert.classList.add("hide");
-		var failAlert = document.getElementById('fail-alert-box');
-		failAlert.classList.remove("hide");
+		showAnswerResultAlert(false);
 	}
-	
-	questionNumber += 1;
-	document.getElementById('answer').value = "";
-	getQuestion(questionNumber)	
 }
 
+function getTodayAsString() {
+	var d = new Date();
+	var days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+	return days[d.getDay()];
+}
 
+function showAnswerResultAlert(result) {
+	var failAlert = document.getElementById('fail-alert-box');
+	var successAlert = document.getElementById('success-alert-box');
 
-function startGame() {
-	var startButton = document.getElementById('start-button');
-	var submitButton = document.getElementById('submit-button');
-	var answrBox = document.getElementById('answer-box');
-	var questionHeading = document.getElementById('question-heading');
-	
-	startButton.classList.add("hide");
-	submitButton.classList.remove("hide");
-	answrBox.classList.remove("hide");
-	questionHeading.classList.remove("hide");
-	
-	var question = document.getElementById('question');
-	question.innerHTML = questionAsked;
+	if (result) {
+		failAlert.classList.add("hide");
+		successAlert.classList.remove("hide");
+	} else {
+		failAlert.classList.remove("hide");
+		successAlert.classList.add("hide");
+	}
+}
+
+function clearAnswerInputField() {
+	document.getElementById('answer').value = "";
+}
+
+function updateRank() {
+	if (questionsCorrect < 5 && questionsCorrect > 2) {
+		rank = 'Silver';
+	} else if (questionsCorrect === 5) {
+		rank = 'Gold';
+	}
+}
+
+function isGameEnded() {
+	return questionNumber >= 5;
+}
+
+function updateNextQuestion() {
+
+	var questions = [
+		"What is the day today?",
+		"What is: 2 + 2 - 1 = ?  (Quick Mafs)",
+		"What animal goes moo?",
+		"How many fingers do humans have on one hand?",
+		"Who sings 'I feel good'?"
+	];
+
+	var question = questions[questionNumber] + getQuestionsCorrectAndRank();
+	document.getElementById('question').innerHTML = question;
+
+	questionNumber += 1;
+}
+
+function getQuestionsCorrectAndRank() {
+	return ' [' + questionsCorrect + ' Correct Answers - Current Rank = ' + rank + ' ]';
 }
 
 function endGame() {
-	var startButton = document.getElementById('start-button');
-	var submitButton = document.getElementById('submit-button');
-	var answrBox = document.getElementById('answer-box');
-	var questionHeading = document.getElementById('question-heading');
-	var successAlert = document.getElementById('success-alert-box');
-	var failAlert = document.getElementById('fail-alert-box');
-	failAlert.classList.add("hide");
-	successAlert.classList.add("hide");
-	
-	startButton.classList.add("hide");
-	submitButton.classList.add("hide");
-	answrBox.classList.add("hide");
-	questionHeading.classList.add("hide");
-	
-	var question = document.getElementById('question');
-	question.innerHTML = questionAsked;
+	displayResults();
+
+	document.getElementById('start-button').classList.add("hide");
+	document.getElementById('submit-button').classList.add("hide");
+	document.getElementById('answer-box').classList.add("hide");
+	document.getElementById('question-heading').classList.add("hide");
+	document.getElementById('success-alert-box').classList.add("hide");
+	document.getElementById('fail-alert-box').classList.add("hide");
+}
+
+function displayResults() {
+	var result;
+
+	if (rank == "Bronze") {
+		result = 'Dam Daniel! Go back to high school! You answered ' + questionsCorrect + ' questions correctly and earned a Rank of - ' + rank;
+	}
+	if (rank == "Silver") {
+		result = 'Not Bad! You answered ' + questionsCorrect + ' questions correctly and earned a Rank of - ' + rank;
+	}
+	if (rank == "Gold") {
+		result = 'Awesome! You answered ' + questionsCorrect + ' questions correctly and earned a Rank of - ' + rank;
+	}
+
+	document.getElementById('question').innerHTML = result;
+}
+
+
+function startGame() {
+	document.getElementById('start-button').classList.add("hide");
+	document.getElementById('submit-button').classList.remove("hide");
+	document.getElementById('answer-box').classList.remove("hide");
+	document.getElementById('question-heading').classList.remove("hide");
+
+	updateNextQuestion();
 }
